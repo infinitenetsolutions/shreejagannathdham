9ff67fbd73dc4a93d0d538ef4c965889db45ea91
ajit <ajit@gmail.com>
2022-01-20 16:59:30 +0530

    updating 20-12

--- index.php
+++ index.php
@@ -319,7 +319,7 @@ include 'admin/connection.inc.php';
                 <h5 class="text-white">
                   <a href=""><?php echo $row['name']; ?></a></h5>
               </div>
-              <a href="volunteer.php?id=<?php echo $row['id']; ?>" class="sigma_btn-custom secondary" style="padding: 10px 20px">Know More <i class="far fa-arrow-right"></i> </a>
+              <a href="volunteer.php?id=<?php echo $row['id']; ?>" class="sigma_btn-custom" style="padding: 10px 20px">Know More <i class="far fa-arrow-right"></i> </a>
             </div>
           </div>
         </div>
